edae5dd36a32ad191933dc86a6d581ed66394147
thekiwidev <adedotungabriel20@gmail.com>
2023-09-30 16:15:21 +0100

    github access token in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
-
-
   return (
     <div className="App">
-     <Header/>
+      <Header />
     </div>
   );
 }
 
 export default App;
 
-
-
+// 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;
